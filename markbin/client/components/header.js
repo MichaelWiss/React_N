02a9a0c78d39a4309da5fddfaa0d8a340a185bc2
MichaelWiss <michael.wiss@gmail.com>
2017-02-15 13:04:56 -0600

    header

--- markbin/client/components/header.js
+++ markbin/client/components/header.js
@@ -5,20 +5,20 @@ class Header extends Component {
 	render() {
 		return (
            <nav className="nav navbar-dafault">
-
-           </nav className="nav navbar-default">
               <div className="navbar-header">
               		<a className="navbar-brand">Markbin</a>
               </div>
               <ul className="nav navbar-nav">
-               <li>
-               	  	<a>Sign up</a>
-               </li>
-               <li>
-               		<a>Create Bin</a>
-               </li>
+	               <li>
+	               	  	<a>Sign up</a>
+	               </li>
+	               <li>
+	               		<a>Create Bin</a>
+	               </li>
                </ul>
             </nav>
 			);
 	}
 }
+
+export default Header;
