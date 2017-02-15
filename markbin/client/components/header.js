5758c6712e7bbe72a78b00d7c4ee5171a7600391
MichaelWiss <michael.wiss@gmail.com>
2017-02-14 19:04:18 -0600

    nav

--- markbin/client/components/header.js
+++ markbin/client/components/header.js
@@ -1,1 +1,13 @@
-import React, { Component }
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+	render() {
+		return (
+           <nav className="nav navbar-dafault">
+
+           </nav
+
+			);
+	}
+}
