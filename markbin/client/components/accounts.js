97dc4f1ae66747b1e0b5e4eb001d3903741827c3
MichaelWiss <michael.wiss@gmail.com>
2017-02-15 13:36:49 -0600

    lifecycle methods

---
+++ markbin/client/components/accounts.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+
+class Accounts extends Component {
+	componentDidMount() {
+		//render the blaze accounts form then find the div
+		//we just rendered in the 'render' method and place
+		// the blaze accounts form in the div
+	}
+
+	componentWillUnmount() {
+		//go find the forms we created and destroy them
+		//we need to clean up those forms ourselves
+	}
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
+
+export default Accounts;
