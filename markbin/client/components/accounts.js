63fc9d04dfdda31b7c6f6700ed894713b636ca31
MichaelWiss <michael.wiss@gmail.com>
2017-02-15 13:53:43 -0600

    using Blaze for account login

--- markbin/client/components/accounts.js
+++ markbin/client/components/accounts.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
 
 
 
@@ -7,15 +10,18 @@ class Accounts extends Component {
 		//render the blaze accounts form then find the div
 		//we just rendered in the 'render' method and place
 		// the blaze accounts form in the div
+		this.view = Blaze.render(Template.loginButtons, 
+			ReactDOM.findDOMNode(this.refs.container));
 	}
 
 	componentWillUnmount() {
 		//go find the forms we created and destroy them
 		//we need to clean up those forms ourselves
+		Blaze.remove(this.view);
 	}
 	render() {
 		return (
-			<div></div>
+			<div ref="container"></div>
 		);
 	}
 }
