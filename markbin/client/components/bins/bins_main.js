0688b9fc98ee52e2b0bdce336d35c3d8893c6abd
MichaelWiss <michael.wiss@gmail.com>
2017-02-21 21:56:54 -0600

    BinsMain to a container

--- markbin/client/components/bins/bins_main.js
+++ markbin/client/components/bins/bins_main.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Bins } from '../../../imports/collections/bins';
 
 class BinsMain extends Component {
 	render() {
@@ -9,4 +11,6 @@ class BinsMain extends Component {
 	}
 }
 
-export default BinsMain;
+export default createContainer(() => {
+
+}, BinsMain);
