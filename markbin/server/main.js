755060c0fcf6b8f41c8ecf5d2111b6f848e984f6
MichaelWiss <michael.wiss@gmail.com>
2017-02-22 20:28:34 -0600

    query array for matching email element

--- markbin/server/main.js
+++ markbin/server/main.js
@@ -8,8 +8,14 @@ Meteor.startup(() => {
 
   Meteor.publish('sharedBins', function() {
      const user = Meteor.users.findOne(this.userId);
-
      
+     if (!user) { return; }
+
+     const email = user.emails[0].address;
+
+     return Bins.find({
+         sharedWith: { $elemMatch: { $eq: email }}
+     });
   });
 });
 
