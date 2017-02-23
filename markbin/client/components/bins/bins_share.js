11aa8192c04ae0efc701a647b2456f1668d3b13b
MichaelWiss <michael.wiss@gmail.com>
2017-02-22 18:54:03 -0600

    onShareClick function

--- markbin/client/components/bins/bins_share.js
+++ markbin/client/components/bins/bins_share.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 class BinsShare extends Component {
+	onShareClick() {
+       const email = this.refs.email.value;
+	}
 	render() {
       return (
       	<footer className="bins-share">
       	   <div className="input-group">
-      		  <input className="form-control" />
+      		  <input ref="email" className="form-control" />
       		 <div className="input-group-btn">
-      		   <button className="btn btn-default">
+      		   <button 
+      		    onClick={this.onShareClick.bind(this)}
+      		    className="btn btn-default">
       		    Share Bin
       		    </button>
       		 </div>
